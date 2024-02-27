b0027d5d67edf4fa968190dc526d4204e078a69c
Aydan <aydan.akbarova@gmail.com>
2024-02-27 09:02:35 +0000

    formatted according to pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
+"""BaseModel class"""
+
 
 import models
 from datetime import datetime
 from uuid import uuid4
 
+
 class BaseModel:
+    """The BaseModel of the project"""
+
     def __init__(self, *args, **kwargs):
+        """Initializing attributes"""
+
         self.id = str(uuid4())
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.created_at = datetime.today()
@@ -19,16 +26,21 @@ class BaseModel:
         else:
             models.storage.new(self)
 
-
     def save(self):
+        """Updating with current datetime"""
+
         self.updated_at = datetime.today()
         models.storage.save()
 
     def __str__(self):
+        """Returns string representation"""
+
         class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def to_dict(self):
+        """Returns dictionary representation"""
+
         dic = self.__dict__.copy()
         dic["__class__"] = self.__class__.__name__
         dic["created_at"] = self.created_at.isoformat()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,28 +1,42 @@
 #!/usr/bin/python3
+"""FileStorage class"""
 
-import json 
+
+import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
+    """Serializes and deserializes to/from JSON file"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Returns the dictionary"""
+
         return FileStorage.__objects
 
     def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+
         obj_class = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(obj_class, obj.id)] = obj
 
     def save(self):
+        """serializes __objects to the JSON file """
+
         serialized = {}
         for key, value in FileStorage.__objects.items():
             serialized[key] = value.to_dict()
         with open(FileStorage.__file_path, 'w') as file:
             json.dump(serialized, file)
-    
+
     def reload(self):
+        """deserializes the JSON file to __objects"""
+
         try:
+
             with open(FileStorage.__file_path) as file:
                 loaded = json.load(file)
                 for o in loaded.values():
