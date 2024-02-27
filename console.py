e6dcac34460dd4b861f785dc322597f2430a8ad0
Aydan <aydan.akbarova@gmail.com>
2024-02-27 15:23:51 +0000

    do_create

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 
 
 import cmd
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """Class HBNBCommand"""
@@ -19,6 +20,22 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Does nothing if empty line"""
         pass
+
+    def do_create(self, arg):
+        """Creates a class instance and prints id"""
+        args = arg.split()
+        class_name = args[0]
+        if not args:
+            print("** class name missing **")
+        elif class_name is not "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            instance = BaseModel()
+            instance.save()
+            print(instance.id)
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
